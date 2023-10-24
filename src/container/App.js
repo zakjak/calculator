f5a0ae9f168bd5472b5f3613549df682b4f02c27
zakjak <zakjak456@gmail.com>
2023-10-24 15:36:11 -0500

    Start of calculator project

---
+++ src/container/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Screen from '../components/Screen';
+import Buttons from '../components/Buttons';
+
+function App() {
+  return (
+    <div className="App">
+      <Screen />
+      <Buttons />
+    </div>
+  );
+}
+
+export default App;
